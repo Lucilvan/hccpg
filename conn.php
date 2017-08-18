aad82ca7a38bf74e130ef40b470b4a1ce797f3ab
lucilvan <lucilvan@hotmail.com>
2017-08-18 20:30:12 -0300

    banco para heroku

--- conn.php
+++ conn.php
@@ -1,7 +1,11 @@
-$hostname_MySql = "localhost";  // Servidor
-$database_MySql = "hccpg"; // banco de dados
-$username_MySql = "root"; // Usuario
-$password_MySql = "";  // senha*/
+$hostname_MySql = "us-cdbr-iron-east-05.cleardb.net";  // Servidor
+$database_MySql = "heroku_3e2f3c7b538415b?reconnect=true"; // banco de dados
+$username_MySql = "bf1886c2887a04"; // Usuario
+$password_MySql = "52d302f7";  // senha
+//$hostname_MySql = "localhost";  // Servidor
+//$database_MySql = "hccpg"; // banco de dados
+//$username_MySql = "root"; // Usuario
+//$password_MySql = "";  // senha*/
 
 //estabelecemos conexão com o banco de dados
 $conn = mysqli_connect($hostname_MySql,$username_MySql,$password_MySql,$database_MySql) or die(mysql_error());
--- novaPrescricao.php
+++ novaPrescricao.php
@@ -16,10 +16,14 @@
 				<p class="title">PRESCRIÇÃO MÉDICA</p>
 				
 				<?php
-					$hostname_MySql = "localhost";  // Servidor
-					$database_MySql = "hccpg"; // banco de dados
-					$username_MySql = "root"; // Usuario
-					$password_MySql = "";  // senha
+					$hostname_MySql = "us-cdbr-iron-east-05.cleardb.net";  // Servidor
+					$database_MySql = "heroku_3e2f3c7b538415b?reconnect=true"; // banco de dados
+					$username_MySql = "bf1886c2887a04"; // Usuario
+					$password_MySql = "52d302f7";  // senha
+					//$hostname_MySql = "localhost";  // Servidor
+					//$database_MySql = "hccpg"; // banco de dados
+					//$username_MySql = "root"; // Usuario
+					//$password_MySql = "";  // senha*/
 				
 					$con=mysqli_connect($hostname_MySql,$username_MySql,$password_MySql,$database_MySql);
 					// Check connection
