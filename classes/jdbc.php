42e5f6041d550bf3a988d15ea2838a5cc53e0d11
lucilvan <lucilvan@hotmail.com>
2017-08-23 11:29:53 -0300

    info banco

--- classes/jdbc.php
+++ classes/jdbc.php
@@ -8,11 +8,11 @@
 
 	//Conectar ao banco
 	function conectar(){
-		//$hostname_MySql = "us-cdbr-iron-east-05.cleardb.net";  // Servidor
-		//$database_MySql = "heroku_3e2f3c7b538415b"; // banco de dados
-		//$username_MySql = "bf1886c2887a04"; // Usuario
-		//$password_MySql = "52d302f7";  // senha
-		$hostname_MySql = "localhost";  // Servidor
+		$hostname_MySql = "us-cdbr-iron-east-05.cleardb.net";  // Servidor
+		$database_MySql = "heroku_3e2f3c7b538415b"; // banco de dados
+		$username_MySql = "bf1886c2887a04"; // Usuario
+		$password_MySql = "52d302f7";  // senha*/
+		/*$hostname_MySql = "localhost";  // Servidor
 		$database_MySql = "hccpg"; // banco de dados
 		$username_MySql = "root"; // Usuario
 		$password_MySql = "";  // senha*/
